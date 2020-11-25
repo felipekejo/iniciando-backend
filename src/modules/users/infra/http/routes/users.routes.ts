bd151631e6c8126843dae2e7df0756bb89b739df
Felipe Yui <felipe_m87@yahoo.com.br>
2020-11-25 14:20:52 +1030

    Arquitetura e DDD

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,45 +1,24 @@
 import { Router } from 'express'
 import multer from 'multer'
 import uploadConfig from '@config/upload'
+import UsersController from '../controllers/UsersController'
+import UserAvatarController from '../controllers/UserAvatarController'
+
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated'
 
-import CreateUserService from '@modules/users/services/CreateUserSrvice'
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService'
+
 
 
 const usersRouter = Router()
 const upload = multer(uploadConfig)
-
-
-usersRouter.post('/', async (request, response) => {
-
-  const { name, email, password } = request.body
-  const createUser = new CreateUserService()
-  const user = await createUser.execute({
-    name,
-    email,
-    password
-  })
+const usersController = new UsersController()
+const userAvatarController = new UserAvatarController()
 
 
 
-  return response.json(user)
+usersRouter.post('/', usersController.create);
 
-})
-
-usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
-
-
-  const updateUserAvatar = new UpdateUserAvatarService()
-
-  const user = await updateUserAvatar.execute({
-    user_id: request.user.id,
-    avatarFilename: request.file.filename
-  })
-
-  return response.json(user)
-
-})
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), userAvatarController.update)
 
 export default usersRouter
