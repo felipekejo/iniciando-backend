a0b251515649fd779373310b92bf6547ea21e025
Felipe Yui <felipe_m87@yahoo.com.br>
2020-12-15 01:22:11 +1030

    test auth

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -32,7 +32,40 @@ describe('AuthenticateUser', () => {
 
 
   });
+  it('should not be able to Authenticate with non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository()
+    const fakeHashProvider = new FakeHashProvider()
+    const authenticateUser = new AuthenticateUserService(fakeUsersRepository, fakeHashProvider)
+    expect(authenticateUser.execute({
 
+      email: 'johnndoe@example.com',
+      password: '123456'
+    })).rejects.toBeInstanceOf(AppError);
+  });
+  it('should not be able to Authenticate with wrong password', async () => {
+    const fakeUsersRepository = new FakeUsersRepository()
+    const fakeHashProvider = new FakeHashProvider()
+    const createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider)
+
+    const authenticateUser = new AuthenticateUserService(fakeUsersRepository, fakeHashProvider)
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johnndoe@example.com',
+      password: '123456'
+    })
+
+
+
+    expect(authenticateUser.execute({
+
+      email: 'johnndoe@example.com',
+      password: 'wrong-password'
+    })).rejects.toBeInstanceOf(AppError);
+
+
+
+  });
 
 
 })
